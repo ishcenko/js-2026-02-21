1d14dab6b44dc3469a17a865acff83b9129e5fbc
ishcenko <a_ishcenko@i.ua>
2026-02-23 09:33:04 +0200

    delete

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// const clients = ["Mango", "Ajax", "Poly", "Kivi"];
+// console.log(clients.includes("Poly"));
+let numbers = [];
+numbers.push(1);
+console.log(numbers);
+
+numbers.push(2);
+console.log(numbers);
+
+numbers.push(3);
+console.log(numbers);
+
+numbers.push(4);
+console.log(numbers);
