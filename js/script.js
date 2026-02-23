b3cd1cefd49784b9afc99da1854f0cbf1daaeeb5
ishcenko <a_ishcenko@i.ua>
2026-02-23 09:48:10 +0200

    pop

--- js/script.js
+++ js/script.js
@@ -12,3 +12,20 @@ console.log(numbers);
 
 numbers.push(4);
 console.log(numbers);
+
+let number = [1, 2, 3, 4];
+numbers.pop(1);
+console.log(numbers);
+
+numbers.pop(2);
+console.log(numbers);
+
+(numbers, pop(3));
+console.log(numbers);
+
+numbers.pop(4);
+console.log(numbers);
+
+function multiply() {
+  console.log(multiply);
+}
